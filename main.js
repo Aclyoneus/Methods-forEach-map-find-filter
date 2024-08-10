e31c5e16dd7733a6544b06ffbb45b7a59dcf294c
Wojciech <wojciechwroobel@gmail.com>
2024-08-10 18:05:24 +0200

    finished ex5 - doubled numbers array function

--- main.js
+++ main.js
@@ -72,3 +72,15 @@ function findNeedleIndex(haystackArray) {
 }
 
 console.log(findNeedleIndex(['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago']));
+
+// 5
+
+function getDoubledNumbers(numberArray) {
+    const doubledNumbersArray = [];
+    numberArray.map(function(number) {
+        return doubledNumbersArray.push(number * 2);
+    })
+    return doubledNumbersArray;
+}
+
+console.log(getDoubledNumbers([1, 2, 3]));
